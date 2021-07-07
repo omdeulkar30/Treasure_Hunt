4828e03f4ef1acf6f563535363df1ab21bc0f4ad
omdeulkar30 <77421763+omdeulkar30@users.noreply.github.com>
2021-07-07 15:41:32 -0400

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+
+    fill("hotpink")
+    textSize(15)
+    text("L A V B R I E A", 100, 50)
+    fill("red")
+    text("Hint: Always changing, not constant !!", 100, 70)
+    // add code for displaying rest of the names and the hints.
+    fill("pink")
+    textSize(15)
+    text("T U F I N O C N", 700, 150)
+    fill("red")
+    text("Hint: Performs a particular task !!", 700, 180)
+
+    fill("purple")
+    textSize(15)
+    text("A B A D S E A T", 100, 300)
+    fill("pink")
+    text("Hint: It stores all information !!", 100, 330)
+
+}
