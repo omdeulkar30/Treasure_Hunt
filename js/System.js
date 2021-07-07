5055a2c969f313383ad8c975ba3a204b352c04dd
omdeulkar30 <77421763+omdeulkar30@users.noreply.github.com>
2021-07-07 15:43:32 -0400

    Add files via upload

---
+++ js/System.js
@@ -0,0 +1,14 @@
+class System {
+
+    constructor() { }
+
+    authenticate(actualCode, enteredCode) {
+        textSize(50);
+        fill("red");
+        text(code, 300, 300)
+        //add code to return true if correct answer entered.
+
+
+    }
+
+}
